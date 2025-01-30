ec4444e4fd59117c27e868090f37c5d68e817eca
DESKTOP-HFQUQSI\Vincent <vincent.cueto@gmail.com>
2025-01-30 23:47:17 +0100

    Fix getSelectedResources() function to work with tree select

--- dev/src/app/shared/services/filters.service.ts
+++ dev/src/app/shared/services/filters.service.ts
@@ -18,94 +18,19 @@ export class FiltersService {
     }));
   }
 
-  /*getTreeResourceOptions(): any[] {
+  getTreeResourceOptions(): any[] {
     return Object.entries(RESOURCE_CATEGORIES).map(([category, resources]) => ({
-      label: this.capitalizeFirstLetter(category), // Convertit 'wood' en 'Wood'
+      label: this.capitalizeFirstLetter(category), 
       key: category, 
-      selectable: true, // Empêche la sélection des catégories
-      children: Object.entries(resources).map(([resourceKey, resourcePath]) => ({
-        label: this.formatLabel(resourceKey), // Convertit 'moonbell' en 'Moon Bell'
-        key: resourceKey
+      selectable: true, 
+      icon: `tree-custom-icon my-icon-${category}`, 
+      children: Object.entries(resources).map(([resourceKey, resourcePath]) => ({          
+        icon: `tree-custom-icon my-icon-${resourceKey.replaceAll('_', '-')}`,
+        label: this.formatLabel(resourceKey),
+        key: resourceKey,
       }))
     }));
-  }*/
-
-    getTreeResourceOptions(): any[] {
-      return Object.entries(RESOURCE_CATEGORIES).map(([category, resources]) => ({
-        label: this.capitalizeFirstLetter(category), 
-        key: category, 
-        selectable: true, 
-        icon: `tree-custom-icon my-icon-${category}`, 
-        children: Object.entries(resources).map(([resourceKey, resourcePath]) => ({          
-          icon: `tree-custom-icon my-icon-${resourceKey.replaceAll('_', '-')}`,
-          label: this.formatLabel(resourceKey),
-          key: resourceKey,
-        }))
-      }));
-
-
-      /*return [
-        {
-            "label": "Work",
-            "icon": "pi pi-folder",
-            "children": [
-                {
-                    "label": "data.json",
-                    "icon": "pi pi-file"
-                },
-                {
-                    "label": "sales.docx",
-                    "icon": "pi pi-file"
-                },
-                {
-                    "label": "presentation.pptx",
-                    "icon": "pi pi-file"
-                }
-            ]
-        },
-        {
-            "label": "Home",
-            "icon": "pi pi-folder",
-            "children": [
-                {
-                    "label": "grocery.word",
-                    "icon": "pi pi-file"
-                },
-                {
-                    "label": "picture.jpeg",
-                    "icon": "pi pi-file"
-                },
-                {
-                    "label": "homeplan.png",
-                    "icon": "pi pi-file"
-                }
-            ]
-        },
-        {
-            "label": "Multimedia",
-            "icon": "pi pi-folder",
-            "children": [
-                {
-                    "label": "infinity-war.mp4",
-                    "icon": "pi pi-file"
-                },
-                {
-                    "label": "you.mp3",
-                    "icon": "pi pi-file"
-                },
-                {
-                    "label": "endgame.mp4",
-                    "icon": "pi pi-file"
-                },
-                {
-                    "label": "MI.mp4",
-                    "icon": "pi pi-file"
-                }
-            ]
-        }
-    ];*/
-
-    }
+  }
   
   /**
    * Capitalise la première lettre d'un mot (ex: "wood" -> "Wood")
